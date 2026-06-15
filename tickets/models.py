c1d1d278db06b8bf70c94aa706b1f1755651889d
Ilya <novikov.ilia@phystech.edu>
2026-06-16 00:30:56 +0300

    initial comment model

--- tickets/models.py
+++ tickets/models.py
@@ -15,3 +15,23 @@ class Ticket(models.Model):
     
     def __str__(self):
         return f"ticket #{self.id} - {self.title}"
+
+class Comment(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='tickets'
+        )
+    
+    ticket = models.ForeignKey(
+        Ticket,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    
+    def __str__(self):
+        return f'comment #{self.id} to {self.ticket}'
