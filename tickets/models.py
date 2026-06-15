255c2ca4818c198cb1ef7b78c000fb6d56c723e7
Ilya <novikov.ilia@phystech.edu>
2026-06-16 00:19:13 +0300

    initial Ticket model

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Ticket(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='tickets'
+        )
+    
+    def __str__(self):
+        return f"ticket #{self.id} - {self.title}"
